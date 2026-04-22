064234750e5b16784e7d62b161cb75fd5afd6a2a
Kavindya <kavindyanethmini249@gmail.com>
2026-04-22 12:46:10 +0530

    Update Frontend

--- frontend/src/Componenets/CourseModules/AdminPanelModules.jsx
+++ frontend/src/Componenets/CourseModules/AdminPanelModules.jsx
@@ -1431,7 +1431,7 @@ const AdminPanel = () => {
                       {newMaterial.type === "pdf" && (
                         <div>
                           <label className="block text-xs font-medium text-gray-700 mb-1">
-                            Pagess <span className="text-red-500">* (1-1000)</span>
+                            Pages <span className="text-red-500">* (1-1000)</span>
                           </label>
                           <input
                             type="number"
